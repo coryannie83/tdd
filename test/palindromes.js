267ac40ff982761ae50198aae63f7d72539c9610
Cory Budday <cory.budday@libertymutual.com>
2017-07-08 23:42:36 -0400

    chai notes

--- test/palindromes.js
+++ test/palindromes.js
@@ -13,7 +13,14 @@ describe('assess()', function() {
   });
 
 //results not working not sure what i am doing wrong:
-  // 
+  //
   // 3) assess() determine if a word is a palindrome returns true or false:
   //    AssertionError: expected 'noon' to equal true
   //     at Context.<anonymous> (test/palindromes.js:10:27)
+
+// ....just downloaded chai arrays
+//http://chaijs.com/plugins/chai-arrays/
+
+// const chai = require('chai');
+// const assertArrays = require('chai-arrays');
+// chai.use(assertArrays);
